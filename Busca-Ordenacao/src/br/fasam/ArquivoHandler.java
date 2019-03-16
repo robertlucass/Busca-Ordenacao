df8b5d8db040316de8e3cd3a9bb8ec7e1bd3a4df
Acer <Acer@DESKTOP-N0DV3S9.mshome.net>
2019-03-16 13:27:57 -0300

    testes

--- Busca-Ordenacao/src/br/fasam/ArquivoHandler.java
+++ Busca-Ordenacao/src/br/fasam/ArquivoHandler.java
@@ -1,5 +1,87 @@
 package br.fasam;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.PrintWriter;
+
 public class ArquivoHandler {
+
+	public static String nome;
+	static int i=0;
+	
+	public static int quantidadeLinhas() {
+		
+		String arquivoLeitura = "./arquivos/English.txt";
+		LineNumberReader linhaLeitura = null;
+		try {
+			linhaLeitura = new LineNumberReader(new FileReader(arquivoLeitura));
+			
+		}catch(FileNotFoundException e2) {
+			e2.printStackTrace();
+		}
+		try {
+			linhaLeitura.skip(arquivoLeitura.length());
+		}catch(IOException e1) {
+			e1.printStackTrace();
+		}
+		int qtdLinha = linhaLeitura.getLineNumber();
+		try {
+			linhaLeitura.close();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return qtdLinha + 1;
+	}
+
+	public static String[] Vetor() {
+		
+		String lista[] = new String[quantidadeLinhas()];
+		int i = 0;
+		
+		nome = "./arquivos/English.txt";
+		
+		try {
+			FileReader arq = new FileReader(nome);
+			BufferedReader Lerarq = new BufferedReader(arq);
+			String linha;
+			
+			while(i<quantidadeLinhas()) {
+				linha = Lerarq.readLine();
+				lista[i] = linha;
+				i++;
+				
+			}
+			
+			arq.close();
+			
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return lista;
+	}
+	
+	public static void Escrever() throws IOException{
+		int i = 0;
+		String Palavras[] = Ordenacao.Palavras();
+		int Numeros[] = Ordenacao.Numeros();
+		FileWriter arq = new FileWriter("./arquivos/English.txt");
+		@SuppressWarnings("resource")
+		PrintWriter gravarArq = new PrintWriter(arq);
+		
+		while (i<quantidadeLinhas()) {
+			gravarArq.printf(Palavras[i]);
+			gravarArq.printf("%d", Numeros[i]);
+			gravarArq.println();
+			
+			i++;
+		}
+		
+	}
 
 }
--- Busca-Ordenacao/src/br/fasam/Busca.java
+++ Busca-Ordenacao/src/br/fasam/Busca.java
@@ -1,5 +1,38 @@
 package br.fasam;
 
-public class Busca {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Busca {
+	public static void main(String[] args) {
+	
+		String nome = null;
+		int i=0;
+		Scanner leia = new Scanner(System.in);
+		String palav = leia.nextLine();
+		leia.close();
+		palav = palav + " " + palav.length()+ " ";
+		String aquivoLeitura = "./arquivos/English.txt";
+		
+		try {
+			FileReader Arq = new FileReader(nome);
+			BufferedReader Lerarq = new BufferedReader(Arq);
+			while(i<ArquivoHandler.quantidadeLinhas()) {
+				if(Lerarq.readLine().equals(palav)) {
+					System.out.println("Linha: %d"+ i+1);
+			}
+				
+			i++;
+				
+			}
+			Arq.close();
+			
+			
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+	}
 }
