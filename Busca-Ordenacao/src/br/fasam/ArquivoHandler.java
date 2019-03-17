f7cf5a8c0e6f3dc4406f524b1e0907fa41f54aa7
Allan Venicio <Allan Venicio@LAPTOP-2P9TNH1T.domain.name>
2019-03-17 15:38:23 -0300

    lendo os arquivos

--- Busca-Ordenacao/src/br/fasam/ArquivoHandler.java
+++ Busca-Ordenacao/src/br/fasam/ArquivoHandler.java
@@ -1,8 +1,33 @@
 package br.fasam;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class ArquivoHandler {
+	public static void main(String[] args) {
 	
-	
+	try {
+		   BufferedReader leitor = new BufferedReader(
+		   new FileReader("./arquivos/English.txt"));
+		   String linha = leitor.readLine();
+		   while ((linha = leitor.readLine()) != null){ 
+                
+		      System.out.println(linha);
+		      linha = leitor.readLine();
+		   }			   
+		  
+		} catch (FileNotFoundException exemple) {
+		   System.out.println("Erro " + exemple);
+		} catch (IOException exemple) {
+		   System.out.println("Erro " + exemple);
+		}
+}
+}
+
+
 		
 
-}
+
