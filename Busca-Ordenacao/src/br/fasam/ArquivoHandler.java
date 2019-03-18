0b48db9ffa7ba937ca53ca493770f0254e598c67
allan1910venicio <48368955+allan1910venicio@users.noreply.github.com>
2019-03-18 19:53:25 -0300

    Update ArquivoHandler.java

--- Busca-Ordenacao/src/br/fasam/ArquivoHandler.java
+++ Busca-Ordenacao/src/br/fasam/ArquivoHandler.java
@@ -1,37 +1,6 @@
 package br.fasam;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class ArquivoHandler {
-	
-	private String arquivo;
-	private int tamanho ;
-	private String vetor;
-	public String getArquivo() {
-		return arquivo;
-	}
-	public void setArquivo(String arquivo) {
-		this.arquivo = arquivo;
-	}
-	public int getTamanho() {
-		return tamanho;
-	}
-	public void setTamanho(int tamanho) {
-		this.tamanho = tamanho;
-	}
-	public String getVetor() {
-		return vetor;
-	}
-	public void setVetor(String vetor) {
-		this.vetor = vetor;
-	}
-	
-	
-	
 		
-
 	}
 
